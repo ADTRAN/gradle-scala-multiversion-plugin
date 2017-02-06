0599fc76aa32044e4c8da3b815350b10412c66cf
Nathan Alderson <nathan.alderson@gmail.com>
2017-02-06 13:55:42 -0600

    Remove unused variable 'buildTasks'

--- src/main/groovy/com/adtran/ScalaMultiVersionPluginExtension.groovy
+++ src/main/groovy/com/adtran/ScalaMultiVersionPluginExtension.groovy
@@ -18,5 +18,4 @@ package com.adtran
 class ScalaMultiVersionPluginExtension {
     String scalaVersionPlaceholder = "%scala-version%"
     String scalaSuffixPlaceholder = "_%%"
-    List<String> buildTasks = ['build']
 }
